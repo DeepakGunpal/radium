cc3ce4a64c2d4e4019d25401a9edcf6fe59a579c
shivam3596 <shivamchauhan3596@gmail.com>
2021-10-08 12:20:09 +0530

    added solution

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -2,6 +2,11 @@ let urlModel=require("../models/urlModel")
 const validUrl = require('valid-url')
 const shortid = require('shortid')
 const baseUrl = 'http:localhost:3000'
+const { promisify } = require("util");
+const {redisClient} = require("../server")
+
+const SETEX_ASYNC = promisify(redisClient.SETEX).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
 
 let postLongURL = async (req,res) => {
     const longString = req.body.longUrl 
@@ -14,11 +19,13 @@ let postLongURL = async (req,res) => {
             })
 
             if (url) {
-                res.status(201).send({status:true,data:url,msg:"already exists"})
+                res.status(201).send({status:true,data:url})
             } else {
                
                 const shortString = baseUrl + '/' + urlNewCode
                 let newURL = await urlModel.create({longUrl:longString,shortUrl:shortString,urlCode:urlNewCode})
+
+                await SETEX_ASYNC(urlNewCode, 60 * 60, longString);
                 res.status(201).send({ status: true, data: newURL})
                 
             }
@@ -35,21 +42,23 @@ let postLongURL = async (req,res) => {
 
 let shorteningURL = async (req,res) => {
     try {
-        const url = await urlModel.findOne({
-            urlCode: req.params.code
-        })
-        if (url) {
-           
-            return res.redirect(url.longUrl)
-        } else {
-            return res.status(404).send({status:false,msg:'No URL Found'})
-        }
+        let cacheUrl = await GET_ASYNC(req.params.code);
 
-    }
-  
-    catch (error) {
-       
-    res.status(500).send({ status: false, msg: error.message })
+        if(cacheUrl){
+            return res.redirect(cacheUrl)
+        }else{
+            const url = await urlModel.findOne({
+                urlCode: req.params.code
+            })
+            if (url) {
+               
+                return res.redirect(url.longUrl)
+            } else {
+                return res.status(404).send({status:false,msg:'No URL Found'})
+            }
+        }
+    }catch (error) {
+        res.status(500).send({ status: false, msg: error.message })
     }
 
 }
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,64 +1,9 @@
 const express = require('express');
-
-const movieController = require('../controllers/movieController')
-const userController = require('../controllers/userController')
 const urlController = require('../controllers/urlController')
-const fsController=require('../controllers/fsController')
-const mongoose=require('mongoose')
-const jwt=require('jsonwebtoken')
 const router = express.Router();
-
-
-
-let tokenVerification = (req,res,next) =>{
-    try{
-
-    let token=req.header('x-auth-token')
-    if(token) {
-        let validatedToken=jwt.verify(token,'mysecretkey')
-
-        if(validatedToken){
-               req.validatedToken=validatedToken;
-               next()
-        } else {
-            res.status(401).send({ status: false, msg: "invalid token" })
-}
-    } else {
-        res.status(400).send({ status: false, msg: "request header must contain token" })
-    }
-        
-    } catch(error){
-        res.status(500).send({status: false, msg: error.message})
-    }
-
-}
-
-// User routes
-
-router.post('/api/user',userController.registerUser)
-router.get('/api/getUsers',userController.getUsers)
-router.post('/api/userLogin',userController.userLogin)
-//router.get('/api/getUserInfo/:_id"',userController.getUserInfo)
-router.get('/api/users/:userId',tokenVerification, userController.getUserInfo)
-router.put('/api/putUserInfo/:userId',tokenVerification,userController.putUserInfo)
-
-
-// Movie routes
-router.get('/api/movies',movieController.getMovie)
-router.post('/api/postMovies',movieController.createMovie)
-router.put('/api/movies/:movieID',movieController.putMovie)
-router.delete('/api/movies/:movieID',movieController.deleteMovie) // fix update of rest of the fiuels
 
 //url shortening
 router.post('/api/shorteningURL',urlController.postLongURL)
-router.get('/api/url/:code',urlController.shorteningURL)
-
-//fs routes
-router.post('/api/write-file',fsController.writeFiles)
-router.post('/api/write-file-assets',fsController.writeFileInDirectory)
-
-
-
-
+router.get('/:code',urlController.shorteningURL)
 
 module.exports = router;
