ccd62e926dd7b762bcf4edd1de5b7ec416798141
Pritesh Kumar <pritesh1908@gmail.com>
2021-11-24 16:20:29 +0530

    Revert "try catch and intro to status codes"
    This reverts commit 352d914d73b4c69da9a497bed3b6d739fb1ec5bc.

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -2,25 +2,9 @@ const BookModel = require("../models/bookModel.js");
 const mongoose = require("mongoose");
 
 const createBook = async function (req, res) {
-
-  try { 
-    
-    const book = req.body; // book = { }
-    let savedBook = await BookModel.create(book); //  .create ( { } )
-    // if error above, execution will break here and move to catch
-    res.status( 200 ).send({ msg: savedBook });
-
-  }
-  catch ( err )
-  {
-    
-    console.log("This is the error: ", err)
-    res.status(400).send(  { msg: "Please check your input", error: err}  )
-
-    //200-  PERFECT
-    //400-  ERROR
-  }
-
+  const book = req.body;
+  let savedBook = await BookModel.create(book);
+  res.send({ msg: savedBook });
 };
 
 const getBooksData = async function (req, res) {
